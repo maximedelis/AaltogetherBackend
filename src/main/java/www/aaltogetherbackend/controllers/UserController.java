bb19354da8593b532928cf3c55fcd22a4603c7a9
Maxime Delis <maxime.delis@proton.me>
2024-09-08 20:28:26 +0300

    User controller to try JWT auth

---
+++ src/main/java/www/aaltogetherbackend/controllers/UserController.java
@@ -0,0 +1,18 @@
+package www.aaltogetherbackend.controllers;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @GetMapping("/me")
+    public String me() {
+        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        return "Hello, user!";
+    }
+
+}
